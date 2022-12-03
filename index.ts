4d18231331bdc0185ebd393521c6a64b3492f9e7
nam.dao <nam.dao@kobizo.de>
2022-12-03 14:40:04 +0700

    Create db connection

--- index.ts
+++ index.ts
@@ -1,13 +1,25 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { resolve } from 'path';
+import bodyParser from 'body-parser';
 
 dotenv.config({
     path: resolve(__dirname, '../.env'),
 });
 
+import { db } from './db';
+
 const app: Express = express();
 const port = process.env.PORT;
+
+db.connect();
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('App is currently running');
